f8d1feced2fc8adbb53c7fffca27ffdc0304c39c
blackhammerbrewing <michael@blackhammerbrewing.com>
2020-05-21 07:35:27 -0700

    game type system implementation

--- README.md
+++ README.md
@@ -6,4 +6,21 @@ The bot works by connecting to a live twitch chat and responding to user input.
 This allows the community to interact with the stream and the streamers to
 interact with the community in a seemless way.
 
+# Use
+Use !set_mode 'voting' to enter the voting mode then use !init to start the game.
+To end the game and have the bot respond requires using the !end function.
+
+# Config File
+All private information is stored in a file called config.py. This is a private file in a folder called configs. This stores OAuth and ClientID information.
+
+Example configs/config.py
+```python
+TMI_TOKEN = "oauth:sometokenhere"
+CLIENT_ID = "yourclientidhere"
+BOT_NICK = "accountnamehere"
+BOT_PREFIX = "!"
+CHANNEL = ["channelnamehere"]
+ALLOWED_MODS = ["allowed", "users", "to", "interact"]
+ALLOWED_GAMES = ["none", "voting", "trivia"]
+```
 Author: Michael Parkinson 2020
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Execute the Twitch Bot.
 python3 main.py
 """
 from configs.config import TMI_TOKEN, CLIENT_ID, BOT_NICK
-from configs.config import BOT_PREFIX, CHANNEL, ALLOWED_MODS
+from configs.config import BOT_PREFIX, CHANNEL, ALLOWED_MODS, ALLOWED_GAMES
 from twitchio.ext import commands
 from src.random import roll_int
 from src.games import voting
@@ -15,6 +15,7 @@ class Bot(commands.Bot):
 
     def __init__(self):
         self._GAME_ON = False
+        self._GAME_TYPE = ALLOWED_GAMES[0]
         self._WINNERS = {}
         super().__init__(
             irc_token=TMI_TOKEN,
@@ -39,7 +40,14 @@ class Bot(commands.Bot):
         driven events.
         """
         if self._GAME_ON is True:
-            self._WINNERS = voting(self._WINNERS, ctx.author.name, ctx.content)
+            if self._GAME_TYPE == ALLOWED_GAMES[1]:
+                self._WINNERS = voting(
+                    self._WINNERS,
+                    ctx.author.name,
+                    ctx.content
+                    )
+            else:
+                pass
         await self.handle_commands(ctx)
 
     @commands.command(name='greet')
@@ -52,6 +60,27 @@ class Bot(commands.Bot):
         if value is not None:
             await ctx.channel.send(f"{ctx.author.name} rolled a {value}")
 
+    @commands.command(name='list_modes')
+    async def list_modes(self, ctx):
+        string = ""
+        if ctx.author.name in ALLOWED_MODS:
+            for game in ALLOWED_GAMES:
+                game_len = len(game)
+                string += game
+                for i in range(9-game_len):
+                    string += "-"
+                string += " "
+            await ctx.channel.send(str(string))
+
+    @commands.command(name='set_mode')
+    async def set_mode(self, ctx):
+        if ctx.author.name in ALLOWED_MODS:
+            content = ctx.content.split(' ')
+            game_mode = content[1]
+            if game_mode in ALLOWED_GAMES:
+                await ctx.channel.send(f"GAME: {game_mode} is OK")
+                self._GAME_TYPE = game_mode
+
     @commands.command(name='init')
     async def init(self, ctx):
         if ctx.author.name in ALLOWED_MODS:
@@ -61,9 +90,11 @@ class Bot(commands.Bot):
     @commands.command(name='end')
     async def end(self, ctx):
         if ctx.author.name in ALLOWED_MODS:
-            value, count = Counter(self._WINNERS.values()).most_common(1)[0]
-            await ctx.channel.send(f"BEST ANSWER: {value}")
-            self._WINNERS = {}
+            if self._GAME_TYPE is ALLOWED_GAMES[1]:
+                votes = self._WINNERS.values()
+                value, count = Counter(votes).most_common(1)[0]
+                await ctx.channel.send(f"ANSWER: {value} with {count} votes")
+                self._WINNERS = {}
             self._GAME_ON = False
             await ctx.channel.send(f"GAME ON: {self._GAME_ON}")
 
@@ -72,5 +103,5 @@ if __name__ == "__main__":
     bot = Bot()
     try:
         bot.run()
-    except KeyboardInterrupt as e:
-        raise e
+    except KeyboardInterrupt:
+        pass
